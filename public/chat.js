623b83356cde15686af0808647e06d8013744f41
simham <simham@simham-Satellite-C640.(none)>
2014-03-24 07:16:28 +0530

    div thing

--- public/chat.js
+++ public/chat.js
@@ -29,17 +29,25 @@ window.onload = function(){
 	});
 
 	function addMessage(from,text){
-		var li = document.createElement('li');
-		li.className = 'message';
+		var inputDiv = document.createElement('div');
+		inputDiv.className = 'message';
+		inputDiv.innerHTML = '<b>' + from + '</b> : ';
+		var len = text.length;
+		if()
 		if(from == "me"){
-			li.style.float='left';
-			li.innerHTML = '<b>' + from + '</b> : ' + text+'<br>';
+			inputDiv.className= inputDiv.className+" floatLeft";
+			//inputDiv.style.float='left';
+			inputDiv.innerHTML = inputDiv.innerHTML+ text;
 		}
 		else{
-			li.style.float='right';
-			li.innerHTML = text+ ': <b>' + from + '</b><br>';
+			inputDiv.className= inputDiv.className+" floatRight";
+			//inputDiv.style.float='right';
+			inputDiv.innerHTML =inputDiv.innerHTML+ text;
 		}
-		document.getElementById('messages').appendChild(li);
+		var br = document.createElement('br');
+		document.getElementById('messages').appendChild(inputDiv);
+		document.getElementById('messages').appendChild(br);
+		
 	}
 
 	var input = document.getElementById('input');
@@ -104,7 +112,6 @@ function initiateChatWith(name){
 		input.id = chatDiv.id + '-input';
 		input.type='text';
 		input.class = 'textInput';
-		input.style.width='75%';
 		var sendButton = document.createElement('button');
 		sendButton.innerText='Send';
 		footerDiv.appendChild(input);
@@ -129,10 +136,21 @@ function initiateChatWith(name){
 	
 function addPrivateMessage(divId,from,text){
 
-	var li = document.createElement('li');
-	li.className = 'message';
-	li.innerHTML = '<b>' + from + '</b> : ' + text;
-	document.getElementById(divId+'-messages').appendChild(li);
+	var inputDiv = document.createElement('div');
+	inputDiv.className = 'message';
+	if(from == "me"){
+			inputDiv.className= inputDiv.className+" floatLeft";
+			//inputDiv.style.float='left';
+			inputDiv.innerHTML = '<b>' + from + '</b> : ' + text;
+		}
+		else{
+			inputDiv.className= inputDiv.className+" floatRight";
+			//inputDiv.style.float='right';
+			inputDiv.innerHTML = '<b>' + from + '</b>:'+text;
+		}
+	document.getElementById(divId+'-messages').appendChild(inputDiv);
+	var br = document.createElement('br');
+	document.getElementById(divId+'-messages').appendChild(br);
 
 }
 
